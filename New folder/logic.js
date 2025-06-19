d80833e8a00deb26b54168229fff293b749c7f62
Rakshana0228 <rakshanathangamuthu@gmail.com>
2025-06-19 21:10:54 +0530

    first commit

---
+++ New folder/logic.js
@@ -0,0 +1,18 @@
+let a=10;
+let b=20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a++); // Postfix increment
+console.log(++a); // Prefix increment
+console.log(a--); // Postfix decrement
+console.log(--a); // Prefix decrement
+console.log(a===b); // Strict equality
+console.log(a!==b); // Strict inequality
+console.log(a==b); // Loose equality
+console.log(a!=b); // Loose inequality
+console.log(a<b); // Less than
+ 
